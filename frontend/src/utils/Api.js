961657953ae8c7ecf58799068c3d98f9924a5934
Anton Morohin <AMorohin@yandex.ru>
2023-09-09 06:51:36 +0300

    frontend

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -96,6 +96,6 @@ class Api {
 }
 
 export const api = new Api({
-    url: 'api.mesto92.students.nomoredomainsicu.ru',
+    url: 'https://api.mesto92.students.nomoredomainsicu.ru',
 });
 
--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'api.mesto92.students.nomoredomainsicu.ru';
+export const BASE_URL = 'https://api.mesto92.students.nomoredomainsicu.ru';
 
 function checkResponse(res) {
         if (res.ok) {
